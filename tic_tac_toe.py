84d2d22b3554d4613f1be3032767b12b51619414
CloverFive <81444552+CloverFive@users.noreply.github.com>
2022-06-13 03:23:31 +0000

    works

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,175 +1,146 @@
 from random import randint
-# A fun game that plays like this: 
-# H A N G M A N
-#  +---+
-#  |
-#  |
-#  |
-#  ===
-# Missed letters:
-# ___
-# Guess a letter.
+from random import choice
 
-hangman_pics = ['''
-  +---+
-      |
-      |
-      |
-     ===''', '''
-  +---+
-  O   |
-      |
-      |
-     ===''', '''
-  +---+
-  O   |
-  |   |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|   |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|\  |
-      |
-     ===''', '''
-  +---+
-  O   |
- /|\  |
- /    |
-     ===''', '''
-  +---+
-  O   |
- /|\  |
- / \  |
-     ===''', '''
-  +---+
- [O   |
- /|\  |
- / \  |
-     ===''', '''
-  +---+
- [O]  |
- /|\  |
- / \  |
-     ===''']
+default = ' '
+grid = {7: default, 8: default, 9: default, 4: default, 5: default, 6: default, 1: default, 2: default, 3: default}
 
+def display_grid():
+    print(f'''{grid[7]}|{grid[8]}|{grid[9]}
+-+-+-
+{grid[4]}|{grid[5]}|{grid[6]}
+-+-+-
+{grid[1]}|{grid[2]}|{grid[3]}''')
 
-def make_random_word():
-    words = '''ant baboon badger bat bear beaver camel cat clam cobra cougar
- coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
- lion lizard llama mole monkey moose mouse mule newt otter owl panda
- parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep
- skunk sloth snake spider stork swan tiger toad trout turkey turtle
- weasel whale wolf wombat zebra'''.split()
+def take_turn_user(user_preference):
 
-    random_number = randint(1, len(words))
-
-    return words[random_number]
-
-# make a function that asks for a lettter 
-def ask_guess():
-    guess = input("Guess a Letter\n")
-    check_guess(guess)
-
-# make a function that checks if it's right that will also out put a boolen for wether the  inputed letter is cprret or not
-def check_guess(letter):
-
-    if letter in random_word and letter not in correct_guesses:
-        correct_guesses.append(letter)
-
-    elif letter in correct_guesses:
+    while True:
         
-        print('You have already guessed that letter. Choose again.')
-        ask_guess()
+        user_move = int(input('what is your move(1-9)\n'))
+        if grid[user_move] == ' ':
+
+            grid[user_move] = user_preference
+            return
+
+def check_posible_win(computer_simbol, checked_simbol):
+    posible_winning_cominations = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [7, 4, 1], [8, 5, 2], [9, 6, 1], [7, 5, 3],  [9, 5, 1]]
+
+    for i in posible_winning_cominations:
         
+        if grid[i[0]] == checked_simbol and grid[i[1]] == checked_simbol and grid[i[2]] == ' ':
+            grid[i[2]] = computer_simbol
+            return True
+
+        if grid[i[0]] == checked_simbol and grid[i[1]] == ' ' and grid[i[2]] == checked_simbol:
+            grid[i[1]] = computer_simbol
+            return True
+
+        if grid[i[0]] == ' ' and grid[i[1]] == checked_simbol and grid[i[2]] == checked_simbol:
+            grid[i[0]] = computer_simbol
+            return True
+
+def check_corner(computer_simbol):
+    corners = [7, 9, 1, 3]
+    posible_corners = []
+    global grid
     
+    for i in corners:
+        if grid[i] == ' ':
+            posible_corners.append(i) 
+
+    grid[choice(posible_corners)] = computer_simbol
+
+def take_turn_computer(computer_simbol, player_simbol):
+
+    if check_posible_win(computer_simbol, computer_simbol):
+        return
+
+    elif check_posible_win(computer_simbol, player_simbol):
+        return
+        
+    elif grid[5] == ' ':
+        grid[5] = computer_simbol
+        return
+        
+    elif grid[7] == ' ' or grid[9] == ' ' or grid[1] == ' ' or grid[3] == ' ':
+        check_corner(computer_simbol)
+        return
     else:
-        incorrect_guesses.append(letter)
 
-# make a function that will update to a the hangman picture
-# def update_hangman_v1():
-    
-#     print('Missed letters:', end= ' ')
-#     for i in range(len(incorrect_guesses)):
-#         print(incorrect_guesses[i], end= '')
-#     print('\n')
+        posible_moves = []
+        for i in range(9):
+            if grid[i + 1] == ' ':
+                posible_moves.append(i + 1)
+        grid[choice(posible_moves)] = computer_simbol
+        print(posible_moves)
+        return    
 
-#     if len(correct_guesses) >= 1:
-#         for i in range(len(random_word)):
-#             print(i)
-#             for j in range(len(correct_guesses)):
-            
-#                 if random_word[i] == correct_guesses[j]:
-#                     print(correct_guesses[j], end= '')
-#                 else:
-#                     print('_', end= '')
-#     else:
-#         for i in range(len(random_word)):
-#             print('_', end= '')
-#         print('\n')
-
-def update_hangman_v2():
-
-    print('Missed letters:', end= ' ')
-    for i in range(len(incorrect_guesses)):
-        print(incorrect_guesses[i], end= ', ')
-    print('\n')
-    
-    for i in range(len(random_word)):
-        if random_word[i] in correct_guesses:
-            print(random_word[i], end= '')
-        else:
-            print('_', end= '')
-    print('\n')
-
-def check_complete():
-    for i in random_word:
-        if i not in correct_guesses:
-            return False
         
-    return True
+def check_win(simbol):
+    
+    winning_combinations = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [7, 4, 1], [8, 5, 2], [9, 6, 1], [7, 5, 3],  [9, 5, 1]]
+    
+    for i in winning_combinations:
+        if grid[i[0]] == simbol and grid[i[1]] == simbol and grid[i[2]] == simbol:
+            return True
 
-def display_hangman():
-    print(hangman_pics[len(incorrect_guesses)])
+def check_grid_full():
+    if grid[7] != ' ' and grid[8] != ' ' and grid[9] != ' ' and grid[4] != ' ' and grid[5] != ' ' and grid[6] != ' ' and grid[1] != ' ' and grid[2] != ' ' and grid[3] != ' ':
+        return True
 
 def start_new_game():
-    global random_word
-    global correct_guesses
-    global incorrect_guesses
-    
-    random_word = make_random_word()
-    correct_guesses = []
-    incorrect_guesses = []
-    print('H A N G M A N')
 
-def play_hangman():
-    # make an infinint while loop
-    start_new_game()
-    
-    while True:
-        display_hangman()
-        update_hangman_v2()
-        ask_guess() 
-    
-        if check_complete():
-            
-            print(f'Yes! The secret word is \"{random_word}\"! You have won!')
-            if input('Do you want to play again? (yes or no)\n') != 'yes':
-                   
+    user_preference = input('Do you want to be X or O\n')
+    if user_preference == 'X':
+        computer_simbol = 'O'
+    else:
+        computer_simbol = 'X'
+
+    random_number = randint(1, 2)
+
+    if random_number == 1:
+        print('The computer is going first')
+        while True:
+            take_turn_computer(computer_simbol, user_preference)
+            display_grid()
+            print('\n')
+            if check_win(computer_simbol):
+                print('the computer won')
                 break
-            else:
-                start_new_game()
-                
-        if len(incorrect_guesses) == len(hangman_pics) - 1:
-            
-            display_hangman()
-            print('you failed boo you! I am not telling you the secret word! Haha')
-            if input('Do you want to play again? (yes or no)\n') != 'yes':
-                   
+            elif check_grid_full():
+                print('its a tie')
                 break
-            else:
-                start_new_game()
+
+            take_turn_user(user_preference)
+            display_grid()
+            print('\n')
+            if check_win(user_preference):
+                print('the computer won')
+                break
+            elif check_grid_full():
+                print('its a tie')
+                break   
+
+    if random_number == 2:
+
+        print('You are going first')
+        while True:
+    
+            take_turn_user(user_preference)
+            display_grid()
+            print('\n')
+            if check_win(user_preference):
+                print('the computer won')
+                break
+            elif check_grid_full():
+                print('its a tie')
+                break
+
+            take_turn_computer(computer_simbol, user_preference)
+            display_grid()
+            print('\n')
+            if check_win(computer_simbol):
+                print('the computer won')
+                break
+            elif check_grid_full():
+                print('its a tie')
+                break
